6944bae09ccb3e600859e05404d3a0bc1aeea067
Alex Kouadio <ST10120876@vcconnect.edu.za>
2023-06-07 14:56:40 +0200

    fourth

---
+++ app/src/main/java/com/example/part_2_group_work/entrylist.kt
@@ -0,0 +1,13 @@
+package com.example.part_2_group_work
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class entrylist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrylist)
+
+
+    }
+}
